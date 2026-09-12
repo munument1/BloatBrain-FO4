550f63e2255c3ee74d964e7e5b035fe30f24fedd
Party1noodle <ltul9009@gmail.com>
2026-09-12 10:07:44 +0900

    Add CommonLibF4 XMake project

---
+++ f4se-plugin/xmake.lua
@@ -0,0 +1,22 @@
+includes("lib/commonlibf4")
+
+set_project("BloatBrain-FO4")
+set_version("0.1.0")
+set_languages("c++23")
+set_warnings("allextra")
+
+add_rules("mode.debug", "mode.releasedbg")
+add_rules("plugin.vsxmake.autoupdate")
+
+target("BloatBrainFO4")
+    add_rules("commonlibf4.plugin", {
+        name = "BloatBrainFO4",
+        author = "munument1",
+        description = "External neural controller bridge for Fallout 4 Bloatflies"
+    })
+
+    add_files("src/**.cpp")
+    add_headerfiles("src/**.h")
+    add_includedirs("src")
+    set_pcxxheader("src/pch.h")
+    add_syslinks("ws2_32")
