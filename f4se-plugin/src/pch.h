9932eb78e498f7e22d79602fc2f1a0309359a35b
Party1noodle <ltul9009@gmail.com>
2026-09-12 10:07:04 +0900

    Add F4SE plugin precompiled header

---
+++ f4se-plugin/src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <RE/Fallout.h>
+#include <F4SE/F4SE.h>
+
+#include <atomic>
+#include <chrono>
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <thread>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
